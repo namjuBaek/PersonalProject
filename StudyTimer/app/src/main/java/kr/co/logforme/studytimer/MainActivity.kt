9b92f9856c410d95d0a0118b7d6cfbe05faa3408
namju <whitenj0125@gmail.com>
2021-11-14 22:07:21 +0900

    master

--- StudyTimer/app/src/main/java/kr/co/logforme/studytimer/MainActivity.kt
+++ StudyTimer/app/src/main/java/kr/co/logforme/studytimer/MainActivity.kt
@@ -3,6 +3,9 @@ package kr.co.logforme.studytimer
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -23,28 +26,15 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.secTimerTextView)
     }
 
+    private val nowTimeHandler = NowTimeHandler()
+    private lateinit var timeThread: UpdateTimeThread
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initCountTypeButton()
-        settingNowTime()
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun settingNowTime() {
-        val time = getTime()
-        timerTextView.text = time.substring(0, 2) + ":" + time.substring(2, 4)
-        secTimerTextView.text = time.substring(4, 6)
-    }
-
-    private fun getTime(): String {
-        val mNow = System.currentTimeMillis()
-        val date = Date(mNow)
-
-        val dateFormat = SimpleDateFormat("HH:mm:ss")
-
-        return dateFormat.format(date)
+        updateNowTime()
     }
 
     private fun initCountTypeButton() {
@@ -53,16 +43,74 @@ class MainActivity : AppCompatActivity() {
                 0 -> {
                     countType = 1
                     countTypeButton.text = "COUNT-UP"
+                    stopNowTime()
                 }
                 1 -> {
                     countType = 2
                     countTypeButton.text = "COUNT-DOWN"
+                    stopNowTime()
                 }
                 2 -> {
                     countType = 0
                     countTypeButton.text = "CLOCK"
+                    updateNowTime()
                 }
             }
         }
+    }
+
+    private fun updateNowTime() {
+        timeThread = UpdateTimeThread()
+        timeThread.threadStop(false)
+        timeThread.start()
+    }
+
+    private fun stopNowTime() {
+        timeThread.threadStop(true)
+    }
+
+    inner class UpdateTimeThread : Thread() {
+        var stopFlag = false
+
+        override fun run() {
+            while (!stopFlag) {
+                val message = nowTimeHandler.obtainMessage()
+                val bundle: Bundle = Bundle()
+
+                val time = getNowTime()
+                bundle.putString("time", time)
+
+                message.data = bundle
+                nowTimeHandler.sendMessage(message)
+                sleep(1000)
+            }
+        }
+
+        fun threadStop(flag: Boolean) {
+            this.stopFlag = flag
+        }
+    }
+
+    inner class NowTimeHandler : Handler(Looper.getMainLooper()) {
+        @SuppressLint("SetTextI18n")
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+            val bundle: Bundle = msg.data
+            if (!bundle.isEmpty) {
+                //UI Task
+                val time: String = bundle.getString("time").toString()
+                timerTextView.text = time.substring(0, 2) + ":" + time.substring(2, 4)
+                secTimerTextView.text = time.substring(4, 6)
+            }
+        }
+    }
+
+    private fun getNowTime(): String {
+        val mNow = System.currentTimeMillis()
+        val date = Date(mNow)
+
+        val dateFormat = SimpleDateFormat("HHmmss")
+        return dateFormat.format(date)
     }
 }
